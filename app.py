87370157357c3ddb161fc5b16864f2a7dd1e9ebe
Brian Matheson <bmath@brianmatheson.com>
2024-12-20 01:41:47 -0500

    .

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ def index():
                                                          stats['usage']['memory'])
 
     for ing in k8s_ing.items:
-        ingresses += "Ingress: %s / %s: %s<br>", ing.metadata.namespace, ing.metadata.name, ing.spec.rules[0].host
+        ingresses += "Ingress: %s / %s: %s<br>\n", ing.metadata.namespace, ing.metadata.name
                 
     return style_header, menu, "<br>", nodes, ingresses
 

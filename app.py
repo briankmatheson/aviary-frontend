36a6eff60401135c707241578c60a28dee7311b1
Brian Matheson <bmath@brianmatheson.com>
2025-09-26 21:33:59 -0400

    .

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ def ingress_line(ing):
     name = ing.metadata.name
     ip = ing.status.load_balancer.ingress[0].ip,
 
-    return f"<a href=\"\"><tr><td>{ip}\t</td><td>{name}</td><td># {ip}:{serviceport}</td><td>({ns} / {servicename})</td></tr></a>\n"
+    return f"<a href=\"\"><tr><td>{ip[0]}\t</td><td>{name}</td><td># ({ns} / {servicename})</td></tr></a>\n"
 
 @app.route('/')
 def index():

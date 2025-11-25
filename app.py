bd370d25821a9fc80aa3882d417acee4983df72a
Brian Matheson <bmath@brianmatheson.com>
2025-11-25 00:47:53 -0500

    .

--- app.py
+++ app.py
@@ -340,8 +340,8 @@ def index():
             log.append(f"{event.last_timestamp} Event: {event.reason} - {event.message} (Object: {event.involved_object.kind}/{event.involved_object.name})<br>")
         except:
             log.append(f"NO TIMESTAMP Event: {event.reason} - {event.message} (Object: {event.involved_object.kind}/{event.involved_object.name})<br>")
-
-    return style_header, menu, "<br>", nodes, "<br>", ingresses, socket.gethostname(), "@", my_ip, "<hr><br>",  "\n".join(log.sort()),  "<hr></body></html>"
+    log.sort()
+    return style_header, menu, "<br>", nodes, "<br>", ingresses, socket.gethostname(), "@", my_ip, "<hr><br>",  "\n".join(log),  "<hr></body></html>"
 
 
 def main_app():
